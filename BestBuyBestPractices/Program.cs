2e377496516324ae3dab85665858384ad2f1e3ed
TheMrChaptastic <chappy@themrchaptastic.com>
2022-02-07 14:37:23 -0600

    Exercise 1 finished

--- BestBuyBestPractices/Program.cs
+++ BestBuyBestPractices/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Data;
+using System.IO;
+using MySql.Data.MySqlClient;
+using Microsoft.Extensions.Configuration;
+
 
 namespace BestBuyBestPractices
 {
@@ -6,7 +11,26 @@ namespace BestBuyBestPractices
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            string connString = config.GetConnectionString("DefaultConnection");
+            IDbConnection conn = new MySqlConnection(connString);
+
+            var repo = new DapperDepartmentRepository(conn);
+            Console.WriteLine("Type a new Department Name");
+            var newDepartment = Console.ReadLine();
+            Console.WriteLine();
+
+            repo.InsertDepartment(newDepartment);
+            var departments = repo.GetAllDepartments();
+
+            foreach(var d in departments)
+            {
+                Console.WriteLine(d.Name);
+            }
         }
     }
 }
